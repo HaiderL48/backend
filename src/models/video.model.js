1745e0d0507626f5483ed73d1a948f248234d920
Haider48 <haidelimdi@gmail.com>
2024-05-30 17:13:24 +0530

    model update-1

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,9 +1,42 @@
 import mongoose from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema(
    {
-      
+      videoFile: {
+         type: String,
+         required: true,
+         index: true,
+      },
+      thumbnail: {
+         type: String,
+      },
+      owner: {
+         type: Schema.Types.ObjectId,
+         ref: "User",
+      },
+      title: {
+         type: String,
+         required: true,
+      },
+      description: {
+         type: String,
+         required: true,
+         lowercase: true,
+      },
+      duration: {
+         type: Number,
+         required: true,
+      },
+      views: {
+         type: Number,
+      },
+      isPublished: {
+         type: Boolean,
+         default: true,
+      },
    },
    { timestamps }
 );
+videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = mongoose.model("Video", videoSchema);
