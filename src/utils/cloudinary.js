f7e9d29cd9c06017861bf4231cd20b0617c59d2c
Haider48 <haidelimdi@gmail.com>
2024-05-31 17:02:08 +0530

    introduced middleware

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,10 +1,13 @@
-import { v2 } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
 
 cloudinary.config({
    cloud_name: process.env.CLOUDINARY_NAME,
    api_key: process.env.CLOUDINARY_API_KEY,
    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
+
 
 const cloudinaryUpload = async (localFilePath) => {
    try {
